9383303e92f3d57b72e833d7dd310f929f335a42
noelfoka <nomgnenoel@gmail.com>
2024-01-11 13:26:01 +0100

    add two root

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" className='px-10'>Delete</Button>
+      <h1 className='text-4xl'>Evently</h1>
     </main>
   )
 }
