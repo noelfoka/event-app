8ef891fa6bb4c931af53e28ad0ae7d4fccf68153
noelfoka <nomgnenoel@gmail.com>
2024-02-02 18:07:24 +0100

    add style

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
