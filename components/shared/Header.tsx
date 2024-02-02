836aba6ce34648382a4e5a11c9baae5868c26e00
noelfoka <nomgnenoel@gmail.com>
2024-02-02 18:16:47 +0100

    import Link to Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex items-center justify-between"></div>
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/"></Link>
+      </div>
     </header>
   )
 }
