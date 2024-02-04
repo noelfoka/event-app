fb3356df4d0bfe58fabae370922e8b0159c3eee1
noelfoka <nomgnenoel@gmail.com>
2024-02-04 20:11:13 +0100

    importation

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+
 const MobileNav = () => {
   return (
     <div>
