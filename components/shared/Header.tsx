f3621e2fe6b5aae2e5e2fee78a01d0b2772a0d88
noelfoka <nomgnenoel@gmail.com>
2024-02-04 09:26:28 +0100

    add UserButton

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
-import { SignedOut } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 
 const Header = () => {
   return (
@@ -17,6 +17,9 @@ const Header = () => {
         </Link>
 
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
           <SignedOut>
             <Button className="rounded-full" size="lg">
               <Link href="/sign-in">
