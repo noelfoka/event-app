dc0e05b0e0f24ffaa50ee3b90ed403e581053d45
noelfoka <nomgnenoel@gmail.com>
2024-02-16 10:24:30 +0100

    add other section to main

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,6 +29,8 @@ export default function Home() {
           />
         </div>
       </section>
+
+      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'></section>
     </>
   );
 }
