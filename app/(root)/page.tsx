d3f744cbfb0187e48d542be34a268ec3f6dd129e
noelfoka <nomgnenoel@gmail.com>
2024-02-16 10:29:54 +0100

    add style

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,7 +31,9 @@ export default function Home() {
       </section>
 
       <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-        <h2 className=''></h2>
+        <h2 className='h2-bold'>Trust by <br /> Thousands of Events</h2>
+
+        <div className='flew w-full flex-col gap-5 md:flex-row'></div>
       </section>
     </>
   );
