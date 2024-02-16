94cdc85a5cee6816c8c306bdd8957acc133dfd15
noelfoka <nomgnenoel@gmail.com>
2024-02-16 10:31:43 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -33,7 +33,10 @@ export default function Home() {
       <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
         <h2 className='h2-bold'>Trust by <br /> Thousands of Events</h2>
 
-        <div className='flew w-full flex-col gap-5 md:flex-row'></div>
+        <div className='flew w-full flex-col gap-5 md:flex-row'>
+          Search
+          CategoryFilter
+        </div>
       </section>
     </>
   );
