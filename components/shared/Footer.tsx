30631ecd4d6397a2792a723e0644aa37d4f5cd48
noelfoka <nomgnenoel@gmail.com>
2024-02-16 08:54:13 +0100

    finish footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div>
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
           <Image 
             src='/assets/images/logo.svg' 
