610036e51efbba1f2c83554c8d132ac563fa45a5
noelfoka <nomgnenoel@gmail.com>
2024-02-16 07:32:10 +0100

    add style

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,9 +1,17 @@
+import { headerLinks } from '@/constants'
+import Link from 'next/link';
 import React from 'react'
 
 const NavItems = () => {
   return (
-    <ul className='md:flex-between flex'>
-      
+    <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
+      {headerLinks.map((link) => {
+        return (
+          <li>
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
     </ul>
   )
 }
