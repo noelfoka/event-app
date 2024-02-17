34114c7285706764f9183abb6dc4c280ae838ceb
noelfoka <nomgnenoel@gmail.com>
2024-02-17 12:24:23 +0100

    add firstName to schema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,4 +4,5 @@ const UserSchema = new Schema({
   clerkId: { type: "string", required: true, unique: true },
   email: { type: "string", required: true, unique: true},
   username: { type: "string", required: true, unique: true},
+  firstName: { type: "string", required: true } 
 });
