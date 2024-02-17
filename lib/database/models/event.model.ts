89d1aafd81eb6b16a06073e12d20bdda610a6ba6
noelfoka <nomgnenoel@gmail.com>
2024-02-17 14:38:11 +0100

    update2

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,18 +1,19 @@
 import { Schema, Types, model, models } from "mongoose";
 
 export interface IEvent extends Document {
-  title: string,
-  description?: string,
-  location?: string,
-  createdAt: Date,
-  imageUrl: string
-  startDateTime: Date,
-  endDateTime: Date,
-  price?: string,
-  isFree: boolean,
-  url?: string,
-  category: Types.ObjectId | string,
-  organizer: Types.ObjectId | string
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string, name: string };
+  organizer: { _id: string, firstName: string, lastName: string };
 }
 
 const EventSchema = new Schema({
