fa0c11ad6f3332d02932f6614470be287775f870
noelfoka <nomgnenoel@gmail.com>
2024-02-17 12:22:59 +0100

    add username to schema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,4 +3,5 @@ import { Schema } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: "string", required: true, unique: true },
   email: { type: "string", required: true, unique: true},
+  username: { type: "string", required: true, unique: true},
 });
