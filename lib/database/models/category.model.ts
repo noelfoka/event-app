c1e6a67906b75f5fb9c9b59e13b5ab17999259e0
noelfoka <nomgnenoel@gmail.com>
2024-02-19 10:35:11 +0100

    update

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,4 +1,9 @@
-import { Schema } from "mongoose";
+import { Document, Schema } from "mongoose";
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
 
 const CategorySchema = new Schema({
   name: { type:String, required:true, unique: true },
