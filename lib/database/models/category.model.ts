f0c70cfb47f4b40ba9628beef61d1124fd755ee2
noelfoka <nomgnenoel@gmail.com>
2024-02-19 10:40:35 +0100

    create category

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -8,3 +8,5 @@ export interface ICategory extends Document {
 const CategorySchema = new Schema({
   name: { type:String, required:true, unique: true },
 })
+
+const Category = models.Category || model('Category', CategorySchema);
