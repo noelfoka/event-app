6179d23a7549553a7f3dbf80c0d616b604e21c13
noelfoka <nomgnenoel@gmail.com>
2024-02-19 07:50:13 +0100

    try to connect user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,12 @@
 'use server';
 
 import { CreateUserParams } from "@/types";
+import { handleError } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
 
-  } catch (err) {
-    
+  } catch (error) {
+    handleError(error)
   }
 }
